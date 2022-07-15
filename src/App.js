cf10ae1e537785e5c8bac6644d9fc67e5fc46892
kmieciu6 <jakub.kmiecik96@gmail.com>
2022-07-15 14:05:30 +0200

    time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import time from './assets/clock-solid.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        const today = new Date(),
+            time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+        super(props)
+        this.state = {
+            currentTime: time,
+            seconds: 0,
+            text: "seconds ego"
+        }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState((prevState) => {
+                return {
+                    seconds: prevState.seconds + 1
+                };
+            });
+        }, 1000);
+    }
+
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    render() {
+        return (
+            <section className='time'>
+                <h1>{this.state.currentTime}</h1>
+                <div>
+                    <img src={time} alt="time"/>
+                    <h2>{this.state.seconds} {this.state.text}</h2>
+                </div>
+                <button onClick={() => window.location.reload()}>Refresh</button>
+            </section>
+        );
+    }
 }
-
 export default App;
